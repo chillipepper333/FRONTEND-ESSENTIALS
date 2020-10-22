16d5263fc6694f0e47b5528183bfe94f2f5f9b69
Wouter Baas <Wouter.Baas@student.rocva.nl>
2020-10-22 12:05:14 +0200

    essentials 5-5

--- 05 - Javascript/taak01 - Console.log/script.js
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -2,11 +2,16 @@
  let mijnVoornaam = "Wouter";
  let mijnAchternaam = "Baas";
  let mijnLeeftijd = 16;
- let ikBenStemGerechtig = false;
+ let ikBenStemGerechtig = 18;
  let ikMagLegaalAlcoholKopen = false;
  let ikMagBrommerRijden = false;
  let softwareOntwikkeling = null;
+ let stemGerechtigeLeeftijd = 18
 
 
 let spatie = " "
 let voledigeNaam = "Mijn naam is " + mijnVoornaam + spatie + mijnAchternaam +" en mijn leeftijd is " + mijnLeeftijd + spatie +"jaar";
+
+if (mijnLeeftijd == stemGerechtigeLeeftijd){
+    alert ("je mag stemmen");
+}
