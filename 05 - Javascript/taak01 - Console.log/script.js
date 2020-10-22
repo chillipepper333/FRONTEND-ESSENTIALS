f067bf4a58cf767117c99f86e36b0b584306d9e6
Wouter Baas <Wouter.Baas@student.rocva.nl>
2020-10-22 11:46:34 +0200

    essentials 5-4

--- 05 - Javascript/taak01 - Console.log/script.js
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -1,7 +1,12 @@
-let mijnVoornaam = "Wouter";
-let mijnAchternaam = "Baas";
-let mijnLeeftijd = 16;
-let ikBenStemGerechtig = false;
-let ikMagLegaalAlcoholKopen = false;
-let ikMagBrommerRijden = false;
-let softwareOntwikkeling = null;
+
+ let mijnVoornaam = "Wouter";
+ let mijnAchternaam = "Baas";
+ let mijnLeeftijd = 16;
+ let ikBenStemGerechtig = false;
+ let ikMagLegaalAlcoholKopen = false;
+ let ikMagBrommerRijden = false;
+ let softwareOntwikkeling = null;
+
+
+let spatie = " "
+let voledigeNaam = "Mijn naam is " + mijnVoornaam + spatie + mijnAchternaam +" en mijn leeftijd is " + mijnLeeftijd + spatie +"jaar";
