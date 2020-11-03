a5c51bdc42ace7094b5c34a1e28ab2f850819991
Wouter Baas <Wouter.Baas@student.rocva.nl>
2020-11-03 14:06:45 +0100

    essentials eindopdracht 1.1

---
+++ opdracht/Laatste opdracht/script.js
@@ -0,0 +1,52 @@
+//voornaam
+var voornaam = prompt("wat is je voornaam");
+var element = document.getElementById("id01");
+element.innerHTML = voornaam;
+
+//achternaam
+var achternaam = prompt("wat is je achternaam");
+var element = document.getElementById("id02");
+element.innerHTML = achternaam;
+
+//email
+var email = prompt("wat is je email");
+var element = document.getElementById("id03");
+element.innerHTML = email;
+
+//woonplaats
+var woonplaats = prompt("waar woon je ");
+var element = document.getElementById("id04");
+element.innerHTML = woonplaats;
+
+//leeftijd
+var leeftijd = prompt("hoe oud ben je");
+if(leeftijd == 16){
+    document.body.style.backgroundColor = "orange";
+}
+if(leeftijd == 17){
+    document.body.style.backgroundColor = "blue";
+}
+if(leeftijd == 18){
+    document.body.style.backgroundColor = "yellow";
+}
+if(leeftijd == 19){
+    document.body.style.backgroundColor = "groen";
+}
+if(leeftijd== null){
+    document.body.style.backgroundColor = "red";
+}
+
+//console
+let bezoeker ={
+    voornaam: voornaam,
+    achternaam: achternaam,
+    email: email,
+    woonplaats: woonplaats,
+    leeftijd: leeftijd
+}
+console.log(bezoeker)
+
+
+
+
+
